962e6c1b2852d112711386522844fc136e230fa2
garry980318 <radoslav.grencik@gmail.com>
2019-02-10 10:37:08 +0100

    added main

---
+++ OWclient/src/owclient/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author Radoslav Grencik <xgrenc00@stud.fit.vutbr.cz>
+ */
+package owclient;
+
+/**
+ *
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
