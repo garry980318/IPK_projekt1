0d84adb2f10376f41e074b231e1397ca81d38956
garry980318 <radoslav.grencik@gmail.com>
2019-02-11 16:29:46 +0100

    added doc

--- python/OWclient.py
+++ python/OWclient.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# author: Radoslav Grencik <xgrenc00@stud.fit.vutbr.cz>
+
+################################################################################
 
 import sys
 import socket
