4617bf2f6f12d5f093d37f34032257158131910e
garry980318 <radoslav.grencik@gmail.com>
2019-02-11 16:14:59 +0100

    doneeeee

--- python/OWclient.py
+++ python/OWclient.py
@@ -4,13 +4,17 @@ import sys
 import socket
 import json
 
+if len(sys.argv) != 3:
+    print("Err: bad args.")
+    sys.exit(1)
+
 appid = sys.argv[1]
 loc = sys.argv[2]
 
 host = "api.openweathermap.org"
 resource = "/data/2.5/weather?q=" + loc + "&APPID=" + appid +"&units=metric"
 
-req = bytes("GET " + resource + " HTTP/1.1\n" + "Host: " + host + "\n\n", "utf-8")
+req = bytes("GET " + resource + " HTTP/1.1\n" + "Host: " + host + "\r\n\r\n", "utf-8")
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.connect((host, 80))
@@ -19,8 +23,13 @@ result = soc.recv(10000)
 soc.close()
 
 result = str(result, "utf-8")
-result = result.split("\r\n\r\n")[1]
 
+check = result.split("\r\n")[0]
+if check != "HTTP/1.1 200 OK":
+    print("Err: bad response.")
+    sys.exit(1)
+
+result = result.split("\r\n\r\n")[1]
 jres = json.loads(result)
 
 print(
@@ -31,7 +40,7 @@ humidity:   {3} %
 pressure:   {4} hPa
 wind-speed: {5} km/h
 wind-deg:   {6}""".format(jres["name"],
-                          jres["weather"][0]["main"],
+                          jres["weather"][0]["description"],
                           jres["main"]["temp"],
                           jres["main"]["humidity"],
                           jres["main"]["pressure"],
